bda364e48196e26c0385be540df01231c223e924
gentledeking1 <kingsleyokoli45@gmail.com>
2024-09-29 12:46:24 -0700

    second commit

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -1,18 +1,25 @@
 def perform_operation(num1, num2, operation):
-
+   
   match operation:
 
     case 'add':
       return num1 + num2
-
+      
     case 'subtract':
       return num1 - num2
 
     case 'multiply':
       return num1 * num2
-
+    
     case 'divide':
 
       if num2 == 0:
         print("You cannot divide by zero.")
         return Null
+      
+      elif num2 != 0:
+        return num1 / num2
+
+    case _:
+      print('Invalid operation')
+      return Null
