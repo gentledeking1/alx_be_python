507eb10e8f4777c2905acef2e88577b110c5958c
gentledeking <kingsleyokoli45@gmail.com>
2024-10-06 22:14:09 +0100

    gentledeking4

---
+++ fns_and_dsa/programming_paradigm/robust_division_calculator.py
@@ -0,0 +1,19 @@
+def safe_divide(numerator, denominator):
+
+  try:
+    numerator = float(numerator)
+    denominator = float(denominator)
+
+    result = numerator / denominator
+
+  except ZeroDivisionError:
+    return ('Error: Cannot divide by zero.')
+
+  except ValueError:
+    return ('Error: Please enter numeric values only.')
+
+  except Exception:
+    return ('Something is wrong')
+
+  else:
+    return (f'The result of the division is {result}')
